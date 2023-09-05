e0d672e1c7aa1af4f3185c1a6cc5f3b4515b3068
Sinjin Swanepoel <sswanepoel@2u.com>
2023-09-05 08:43:24 +0200

    Remove resources

--- demo.tf
+++ demo.tf
@@ -1,16 +1,16 @@
-resource "github_repository" "project1" {
-  name        = "Project1"
-  description = "Code for our Project1 project"
-}
+# resource "github_repository" "NebulaFusion" {
+#   name        = "NebulaFusion"
+#   description = "Your cosmic hub for seamless team collaboration and project management"
+# }
 
-resource "github_team" "pt" {
-  name        = "team-project1"
-  description = "Project1 team members"
-  privacy     = "closed"
-}
+# resource "github_team" "NebulaFusion-DevTeam" {
+#   name        = "NebulaFusion-DevTeam"
+#   description = "The development powerhouse behind NebulaFusion, driving innovation in cosmic collaboration"
+#   privacy     = "closed"
+# }
 
-resource "github_team_repository" "pt-project1" {
-  repository = github_repository.project1.name
-  team_id    = github_team.pt.id
-  permission = "push"
-}
+# resource "github_team_repository" "NebulaFusion-Dev-link" {
+#   repository = github_repository.NebulaFusion.name
+#   team_id    = github_team.NebulaFusion-DevTeam.id
+#   permission = "push"
+# }
